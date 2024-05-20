afad6d12fc1e95c00e1ae2551cde4ea7ba907740
yashdanej <yashdanej2004@gmail.com>
2024-05-20 12:08:16 +0530

    changes

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,14 @@
 const mysql = require("mysql");
 
 const db = mysql.createPool({
+    // connectionLimit: 10,
+    // host: "localhost",
+    // user: "root",
+    // password: "Yash@1212888",
+    // database: "crmdb"
     connectionLimit: 10,
-    host: "localhost",
-    user: "root",
+    host: "127.0.0.1",
+    user: "yash_danej",
     password: "Yash@1212888",
     database: "crmdb"
 });
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
+const PORT = 8000;
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:3001',  // i am doing this because of cookie request
+    origin: 'http://65.0.30.99',  // i am doing this because of cookie request
     credentials: true,
 }));
 app.use(express.json());
@@ -16,6 +17,6 @@ const leadRouter = require("./routes/lead/leadRouter");
 app.use('/api/v1', authRouter);
 app.use('/api/v1/lead', leadRouter);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server started");
 });
